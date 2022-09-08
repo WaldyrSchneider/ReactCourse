e0df2528f747dd024a8b24f2222e636afef3f2b5
waldyr schneider <waldyr.schneider@adp.com>
2022-09-08 15:40:09 -0300

    filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,16 +12,27 @@ const Expenses = (props) => {
         setFilteredYear(selectedYear);
     };
 
+    const filteredExpenses = props.expenses.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+    let expensesContent = <p>No expenses found.</p>;
+
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date} />
+        ));
+    }
+
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                {props.expenses.map((expense) => (
-                    <ExpenseItem
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date} />
-                ))}
+                {expensesContent}
             </Card>
         </div>
     );
